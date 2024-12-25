5211fa1de4012c276aa0526cae7761052cffc2a9
DeepikaaAPA <deepikaarjun@gmail.com>
2024-12-25 12:38:56 +0530

    update:tailwind css classes for all styles

--- src/App.jsx
+++ src/App.jsx
@@ -39,24 +39,26 @@ function App() {
   if (error) return <p>Error: {error.message}</p>;
   return (
     <div
-      className="flex flex-col md:flex-row items-center relative  text-gray-500 p-4 rounded-lg"
+      className="flex flex-col md:flex-row gap-5 items-center relative  text-gray-500 p-4 rounded-lg"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="card  flex flex-col md:flex-row  p-5 flex-shrink-0">
+      <div className="card bg-teal-950 flex flex-col md:flex-row  p-5 flex-shrink-0">
         <div className=" text-center ">
           <img
-            className="rounded-full  border-gray-100 m-auto md:m-2 hover:scale-125"
+            className="rounded-full border-3 border-gray-100 m-auto md:m-2 hover:scale-125"
             src={data.picture.large}
           ></img>
         </div>
-        <div className="details ps-5">
-          <h2 className="text-4xl md:text-5xl font-extrabold">
+        <div className=" ps-5">
+          <h2 className="text-4xl border-b pb-3 md:text-5xl font-extrabold bg-gradient-to-r from-yellow-100 via-green-200 to-blue-200 text-transparent bg-clip-text">
             {data.name.first} {data.name.last}
+        
           </h2>
+      
           <p
-            className={`capitalize text-md mt-2 mb-9 ${
-              data.gender == "female" ? " text-pink-200" : "text-gray-300"
+            className={`capitalize text-md mt-3 mb-3 ${
+              data.gender == "female" ? " text-pink-300" : "text-gray-300"
             }`}
           >
             {data.gender == "female" ? (
@@ -64,19 +66,19 @@ function App() {
             ) : (
               <FontAwesomeIcon icon={faPerson} />
             )}
-            {"   " + data.gender}
+           <span className="text-xs"> {"   " + data.gender}</span>
           </p>
-          <p className="phone">
+          <p className="text-gray-300">
             <FontAwesomeIcon icon={faPhone} size="sm"></FontAwesomeIcon>
             {"   " + data.phone}
           </p>
         </div>
       </div>
       {isHovered && (
-        <div className="lg:absolute top-full md:-top-10 md:left-full ml-4 bg-green-200 p-4 rounded-lg flex flex-col justify-center w-auto text-left divide-y divide-gray-300 shadow-md">
+        <div className="lg:absolute top-full md:-top-10 md:left-full ml-4 bg-green-200 p-4 rounded-lg flex flex-col justify-center min-w-72 text-left divide-y divide-gray-500 shadow-md">
           {" "}
-          <h1 className="text-2xl font-bold mb-4">About</h1>{" "}
-          <div className="flex flex-col divide-y divide-gray-300">
+          <h1 className="text-2xl font-bold mb-4 text-gray-600 ">About</h1>{" "}
+          <div className="flex flex-col divide-y w-auto divide-gray-400">
             {" "}
             <div className="p-4">
               <FontAwesomeIcon icon={faUser} /> {data.login.username}
@@ -89,12 +91,11 @@ function App() {
               <FontAwesomeIcon icon={faBirthdayCake} />
               {formatDate(new Date(data.dob.date))}
             </div>
-            <div className="p-4">
-              {" "}
+            <div className="p-4 w-auto">
               <FontAwesomeIcon icon={faEnvelope} />
               {data.email}
             </div>
-          </div>{" "}
+          </div>
         </div>
       )}
     </div>
