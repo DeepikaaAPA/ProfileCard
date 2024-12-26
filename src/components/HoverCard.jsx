58096808bbfeccdb0fc7eb1c4d8bb132d4ccb98a
DeepikaaAPA <deepikaarjun@gmail.com>
2024-12-26 13:43:47 +0530

    update: layouts

--- src/components/HoverCard.jsx
+++ src/components/HoverCard.jsx
@@ -13,9 +13,10 @@ const HoverCard = ({ data }) => {
   return (
     <>
       <div
-        className="flex flex-col md:flex-row gap-5 items-center relative  text-gray-500 p-4 rounded-lg"
+        className=" mt-16 md:mt-0 flex flex-col md:flex-row gap-5 items-center md:relative  text-gray-500 p-4 rounded-lg"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
+        onClick={() => setIsHovered(!isHovered)}
       >
         <div className="card min-w-72  bg-teal-900 flex flex-col lg:flex-row  p-5 flex-shrink-0">
           <div className=" text-center mt-2 mb-5 md:mr-4 md:mb-0">
@@ -26,7 +27,7 @@ const HoverCard = ({ data }) => {
           </div>
           <div className=" ps-5">
             <h2 className="text-4xl text-center border-b pb-3 lg:text-4xl font-extrabold bg-gradient-to-r from-yellow-300 via-green-200 to-green-300 text-transparent bg-clip-text">
-             {data.name.first} {data.name.last}
+              {data.name.first} {data.name.last}
             </h2>
             {/* conditional styling of text color of gender */}
             <p
